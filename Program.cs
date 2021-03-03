249daa3a9cb5f324ca64638f339bd1f69b2868a5
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2021-03-03 22:07:36 +0300

    lab 2_1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Implement effective mixing of the characters in the string
+using System;
+
+namespace shuffle
+{
+    class Program
+    {
+        static String Shuffle(String s)
+        {
+            var rand = new Random();
+            var cArr = s.ToCharArray();
+            for (var j = cArr.Length - 1; j > 0; j--)
+            {
+                var i = rand.Next(j);
+                char c = cArr[i];
+                cArr[i] = cArr[j];
+                cArr[j] = c;
+            }
+            return new string(cArr);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the line" );
+            string letters = Console.ReadLine();
+            String res = Shuffle(letters);
+            Console.WriteLine(res);
+        }
+    }
+}
